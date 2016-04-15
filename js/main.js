ec5966d73a4313235d99810803a92a02707d8de3
sagehartly <sage.hartly@gmail.com>
2016-04-15 17:29:16 -0400

    Made images scale and rotate when clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $('.header').on('mouseover', function() {
   $('.headerTitle').addClass('colourHeader');
 });
+
+$('.header').on('mouseout', function() {
+  $('.headerTitle').removeClass('colourHeader');
+});
+
+$('.green-section').on('click', function() {
+  $('.pictureFish').toggleClass('pictureActionLeft');
+});
+
+$('.blue-section').on('click', function() {
+  $('.pictureNeedle').toggleClass('pictureActionRight');
+});
+
+$('.grey-section').on('click', function() {
+  $('.pictureLitter').toggleClass('pictureActionLeft');
+});
+
+$('.white-section').on('click', function() {
+  $('.picturePowder').toggleClass('pictureActionRight');
+});
+
+$('.orange-section').on('click', function() {
+  $('.pictureBall').toggleClass('pictureActionLeft');
+});
